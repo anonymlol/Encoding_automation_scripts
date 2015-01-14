567e22ad407c038a48e7f4b3a57d0eca7057c50e
C.A. <anonymlol7@gmail.com>
2015-01-14 12:36:17 +0100

    Create BDEnc Directory: fix typos

--- Create BDEnc Directory.bat
+++ Create BDEnc Directory.bat
@@ -45,7 +45,7 @@ set Episode_Counter=0
 set /a Episode_Counter=Episode_Counter+1
 if %Episode_Counter% LSS 10 set episodeNumber=0%Episode_Counter%
 if %Episode_Counter% GEQ 10 set episodeNumber=%Episode_Counter%
-if not exist "Ep %episodeNumber%" md "Ep "%episodeNumber%"
+if not exist "Ep %episodeNumber%" md "Ep %episodeNumber%"
 if not %Episode_Counter% == %EpisodeCount% goto :EpisodeLoop
 
 set NC_Counter=0
@@ -53,8 +53,8 @@ set NC_Counter=0
 set /a NC_Counter=NC_Counter+1
 if %NC_Counter% LSS 10 set NC_Number=0%NC_Counter%
 if %NC_Counter% GEQ 10 set NC_Number=%NC_Counter%
-if not exist "NCED %NC_Number%" md "NCED "%NC_Number%"
-if not exist "NCOP %NC_Number%" md "NCOP "%NC_Number%"
+if not exist "NCED %NC_Number%" md "NCED %NC_Number%"
+if not exist "NCOP %NC_Number%" md "NCOP %NC_Number%"
 if not %NC_Counter% == %NCCount% goto :NCLoop
 
 set Special_Counter=0
@@ -62,7 +62,7 @@ set Special_Counter=0
 set /a Special_Counter=Special_Counter+1
 if %Special_Counter% LSS 10 set Special_Number=0%Special_Counter%
 if %Special_Counter% GEQ 10 set Special_Number=%Special_Counter%
-if not exist "Special %Special_Number%" md "Special "%Special_Number%"
+if not exist "Special %Special_Number%" md "Special %Special_Number%"
 if not %Special_Counter% == %SpecialCount% goto :SpecialLoop
 
 set OVA_Counter=0
@@ -70,7 +70,7 @@ set OVA_Counter=0
 set /a OVA_Counter=OVA_Counter+1
 if %OVA_Counter% LSS 10 set OVA_Number=0%OVA_Counter%
 if %OVA_Counter% GEQ 10 set OVA_Number=%OVA_Counter%
-if not exist "OVA %OVA_Number%" md "OVA "%OVA_Number%"
+if not exist "OVA %OVA_Number%" md "OVA %OVA_Number%"
 if not %OVA_Counter% == %OVACount% goto :OVALoop
 
 cd "Ep 01"
