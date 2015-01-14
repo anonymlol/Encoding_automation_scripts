3b35b43cd7e9a47c40197d6ac04132f3cc58daa2
C.A. <anonymlol7@gmail.com>
2015-01-14 17:39:07 +0100

    Create BDEnc Dir: additional bugfixes

--- Create BDEnc Dir.bat
+++ Create BDEnc Dir.bat
@@ -6,7 +6,7 @@ set /p FolderName="Enter Showname: "
 @echo.
 
 :RetryEpCount
-set /p EpisodeCount=Enter Number of Episodes:
+set /p EpisodeCount="Enter Number of Episodes: "
 @echo %EpisodeCount%|findstr /xr "[1-9][0-9]* 0" >nul && (
 	@echo. ) || (
 	@echo Not a valid number!
@@ -38,8 +38,9 @@ set /p OVACount="Enter Number of OVAs: "
 )
 
 if not exist "%FolderName%" md "%FolderName%"
-cd %FolderName%
+cd "%FolderName%"
 
+if %EpisodeCount%==0 goto :NC_Step
 set Episode_Counter=0
 :EpisodeLoop
 set /a Episode_Counter=Episode_Counter+1
@@ -48,6 +49,9 @@ if %Episode_Counter% GEQ 10 set episodeNumber=%Episode_Counter%
 if not exist "Ep %episodeNumber%" md "Ep %episodeNumber%"
 if not %Episode_Counter% == %EpisodeCount% goto :EpisodeLoop
 
+
+:NC_Step
+if %NCCount%==0 goto :Special_Step
 set NC_Counter=0
 :NCLoop
 set /a NC_Counter=NC_Counter+1
@@ -57,6 +61,8 @@ if not exist "NCED %NC_Number%" md "NCED %NC_Number%"
 if not exist "NCOP %NC_Number%" md "NCOP %NC_Number%"
 if not %NC_Counter% == %NCCount% goto :NCLoop
 
+:Special_Step
+if %SpecialCount%==0 goto :OVA_Step
 set Special_Counter=0
 :SpecialLoop
 set /a Special_Counter=Special_Counter+1
@@ -65,6 +71,9 @@ if %Special_Counter% GEQ 10 set Special_Number=%Special_Counter%
 if not exist "Special %Special_Number%" md "Special %Special_Number%"
 if not %Special_Counter% == %SpecialCount% goto :SpecialLoop
 
+
+:OVA_Step
+if %OVACount%==0 goto :Last_Step
 set OVA_Counter=0
 :OVALoop
 set /a OVA_Counter=OVA_Counter+1
@@ -73,11 +82,14 @@ if %OVA_Counter% GEQ 10 set OVA_Number=%OVA_Counter%
 if not exist "OVA %OVA_Number%" md "OVA %OVA_Number%"
 if not %OVA_Counter% == %OVACount% goto :OVALoop
 
+:Last_Step
+if not exist "Ep 01" goto :end
 cd "Ep 01"
 if not exist 480.avs @echo #AVCSource("src.dga") or DGSource("src.dgi") > 480.avs
 if not exist 720.avs @echo #AVCSource("src.dga") or DGSource("src.dgi") > 720.avs
 if not exist 1080.avs @echo #AVCSource("src.dga") or DGSource("src.dgi") > 1080.avs
 
+:end
 @echo Done!
 timeout 1 > nul
 REM pause
