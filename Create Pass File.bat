830a48b0dbf916837130586561bd3ca5b952f3b2
C.A. <anonymlol7@gmail.com>
2015-01-17 17:05:07 +0100

    updated description

--- Create Pass File.bat
+++ Create Pass File.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Press Win+R, type "shell:sendto" and put it there. Now you can just right-click and select "Create Pass File"
+REM Press Win+R, type "shell:sendto" and put it there. Now you can just right-click, go to "Send to" and select "Create Pass File"
 REM Make sure your xvid_encraw path is correct. You can find xvid_encraw in your megui folder or in your xvid folder (download and install xvid from xvid.org)
 
 if not exist pass.avs @echo DirectShowSource(%1) > pass.avs
