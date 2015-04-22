d82f48a827bfcd75f92f1708634b5aa5d388dc1b
C.A <anonymlol7@gmail.com>
2015-04-22 09:03:15 +0200

    New script for quick conversions to WebP

---
+++ Convert to WebP.bat
@@ -0,0 +1,13 @@
+@echo off
+REM A short script to convert images to lossless WebP.
+REM To auto-delete the original files after the conversion, uncomment the 2 lines below (line 7 and 9).
+
+:loop
+echo Saving file "%~n1.webp"
+REM set input="%1"
+cwebp -quiet -lossless -q 100 -m 6 -f 0 -metadata icc "%~1" -o "%~n1.webp"
+REM del "%input%"
+shift
+if not "%~1"=="" goto :loop
+
+REM pause
