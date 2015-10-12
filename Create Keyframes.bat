0317f69593a7b83afad087e2c6e730bfd678b85c
anonymlol <anonymlol7@gmail.com>
2015-10-12 11:18:11 +0200

    Replace old pass file script with new one, should be up to 3-4 times faster
    The new script requires SCXvid-standalone and FFmpeg

---
+++ Create Keyframes.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Press Win+R, type "shell:sendto" and put this batch script there. Now you can just right-click, go to "Send to" and select "Create Pass File"
+REM Requirements
+REM * SCXvid-standalone: https://github.com/soyokaze/SCXvid-standalone/releases
+REM * FFmpeg: http://ffmpeg.zeranoe.com/builds/
+
+echo Creating Keyframes...
+set video="%~1"
+set video2="%~n1"
+ffmpeg -i %video% -f yuv4mpegpipe -vf scale=640:360 -pix_fmt yuv420p -vsync drop -loglevel quiet - | SCXvid.exe %video2%_keyframes.txt
+echo Done
+
+REM pause
