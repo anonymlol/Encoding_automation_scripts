6935f6d36874e188db0b4e1fec41361122e44f88
anonymlol <anonymlol7@gmail.com>
2016-04-13 19:12:18 +0200

    Patch Maker: experimental mac and linux code (commented out)

--- Patch Maker.bat
+++ Patch Maker.bat
@@ -12,10 +12,24 @@ xdelta3 -s "%~nx1" "%~nx2" "%~n1.delta"
 move "%~n1.delta" ./Patches
 echo Done.
 
-echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch all.bat"
-echo if not exist "old" md "old" >> ".\Patches\Patch all.bat"
-echo move "%~nx1" .\old >> ".\Patches\Patch all.bat"
-echo move "%~n1.delta" .\old >> ".\Patches\Patch all.bat"
-echo. >> ".\Patches\Patch all.bat"
+echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch [Windows].bat"
+echo if not exist "old" md "old" >> ".\Patches\Patch [Windows].bat"
+echo move "%~nx1" .\old >> ".\Patches\Patch [Windows].bat"
+echo move "%~n1.delta" .\old >> ".\Patches\Patch [Windows].bat"
+echo. >> ".\Patches\Patch [Windows].bat"
+
+REM Mac and Linux scripts below are experimental and probably broken.
+
+REM echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch [Mac].command"
+REM echo ^[ -d old ^]^; ^|^| mkdir old >> ".\Patches\Patch [Mac].command"
+REM echo mv "%~nx1" ./old >> ".\Patches\Patch [Mac].command"
+REM echo mv "%~n1.delta" ./old >> ".\Patches\Patch [Mac].command"
+REM echo. >> ".\Patches\Patch [Mac].command"
+
+REM echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch [Linux].sh"
+REM echo ^[ -d old ^]^; ^|^| mkdir old >> ".\Patches\Patch [Linux].sh"
+REM echo mv "%~nx1" ./old >> ".\Patches\Patch [Linux].sh"
+REM echo mv "%~n1.delta" ./old >> ".\Patches\Patch [Linux].sh"
+REM echo. >> ".\Patches\Patch [Linux].sh"
 
 REM pause
