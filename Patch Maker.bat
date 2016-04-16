d8e41eeacfd207c98ccebd15ceaaada78c7ac1b2
anonymlol <anonymlol7@gmail.com>
2016-04-16 12:58:58 +0200

    Patch Maker: Add Linux & Mac support

--- Patch Maker.bat
+++ Patch Maker.bat
@@ -1,35 +1,30 @@
 @echo off
-
 REM Usage: Select both files, but right-click/send-to (or drag & drop) only the source (unpatched) file!
+
+REM set path to xdelta3.exe
+set dos2unix_binary=C:\YOURPATH\dos2unix.exe
+set xdelta_binary=C:\YOURPATH\xdelta3.exe
 
 cd "%~dp1"
 if not exist "Patches" md "Patches"
-if not exist ".\Patches\xdelta3.exe" copy "xdelta3.exe" ".\Patches"
+if not exist ".\Patches\xdelta3.exe" copy "%xdelta_binary%" ".\Patches"
 
 echo Creating Patch...
-
-xdelta3 -s "%~nx1" "%~nx2" "%~n1.delta"
-move "%~n1.delta" ./Patches
+%xdelta_binary% -s "%~nx1" "%~nx2" "%~n1.delta"
 echo Done.
+move "%~n1.delta" ./Patches
 
-echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch [Windows].bat"
-echo if not exist "old" md "old" >> ".\Patches\Patch [Windows].bat"
-echo move "%~nx1" .\old >> ".\Patches\Patch [Windows].bat"
-echo move "%~n1.delta" .\old >> ".\Patches\Patch [Windows].bat"
-echo. >> ".\Patches\Patch [Windows].bat"
+echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch_Windows.bat"
+echo if not exist "old" md "old" >> ".\Patches\Patch_Windows.bat"
+echo move "%~nx1" .\old >> ".\Patches\Patch_Windows.bat"
+echo move "%~n1.delta" .\old >> ".\Patches\Patch_Windows.bat"
+echo. >> ".\Patches\Patch_Windows.bat"
 
-REM Mac and Linux scripts below are experimental and probably broken.
-
-REM echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch [Mac].command"
-REM echo ^[ -d old ^]^; ^|^| mkdir old >> ".\Patches\Patch [Mac].command"
-REM echo mv "%~nx1" ./old >> ".\Patches\Patch [Mac].command"
-REM echo mv "%~n1.delta" ./old >> ".\Patches\Patch [Mac].command"
-REM echo. >> ".\Patches\Patch [Mac].command"
-
-REM echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch [Linux].sh"
-REM echo ^[ -d old ^]^; ^|^| mkdir old >> ".\Patches\Patch [Linux].sh"
-REM echo mv "%~nx1" ./old >> ".\Patches\Patch [Linux].sh"
-REM echo mv "%~n1.delta" ./old >> ".\Patches\Patch [Linux].sh"
-REM echo. >> ".\Patches\Patch [Linux].sh"
+echo xdelta3 -d -q "%~n1.delta" >> ".\Patches\Patch_Linux_and_Mac.sh"
+echo mkdir -p old >> ".\Patches\Patch_Linux_and_Mac.sh"
+echo mv "%~nx1" ./old >> ".\Patches\Patch_Linux_and_Mac.sh"
+echo mv "%~n1.delta" ./old >> ".\Patches\Patch_Linux_and_Mac.sh"
+echo. >> ".\Patches\Patch_Linux_and_Mac.sh"
+%dos2unix_binary% ".\Patches\Patch_Linux_and_Mac.sh"
 
 REM pause
