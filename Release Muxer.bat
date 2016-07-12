cc83bc80e24bf5f068a1966ff84b1c97d7f7ed01
anonymlol <anonymlol7@gmail.com>
2016-07-12 13:07:23 +0200

    Release Muxer: add crc32 to filename (needs rhash.exe)

--- Release Muxer.bat
+++ Release Muxer.bat
@@ -8,8 +8,10 @@ cd %~p1
 for %%A in ("%CD%") do set "folderName=%%~nxA"
 for %%A in ("%~dp1..") do set "Showname=%%~nxA"
 
+REM Settings
 set group=[Doki]
 set subtitle_trackname=ASS
+set rhashSettings="rhash.exe" --crc32 -e --percents --printf=%%C --output=nul
 
 if %folderName:~0,2%==Ep (
 	set episodeNumber=%folderName:~-2,2%
@@ -39,11 +41,14 @@ REM Mux settings x265
 set MuxEp_720_x265=mkvmerge -o "%group% %enc_720_hevc%"  "--quiet" "(" "%enc_720_hevc%" ")" "--language" "0:eng" "--track-name" "0:%subtitle_trackname%" "--forced-track" "0:no" "-s" "0" "-D" "-A" "-T" "--no-global-tags" "--no-chapters" "(" "%~nx1" ")" "--track-order" "0:0,0:1,1:0" "--title" "%group% %enc_720_hevc%"%chapters_file%%fonts%
 set MuxEp_1080_x265=mkvmerge -o "%group% %enc_1080_hevc%"  "--quiet" "(" "%enc_1080_hevc%" ")" "--language" "0:eng" "--track-name" "0:%subtitle_trackname%" "--forced-track" "0:no" "-s" "0" "-D" "-A" "-T" "--no-global-tags" "--no-chapters" "(" "%~nx1" ")" "--track-order" "0:0,0:1,1:0" "--title" "%group% %enc_1080_hevc%"%chapters_file%%fonts%
 
-if exist "%enc_480_h264%" if not exist "%group% %enc_480_h264%" @echo Muxing "%group% %enc_480_h264%" && %MuxEp_480_x264% 
-if exist "%enc_720_hi10%" if not exist "%group% %enc_720_hi10%" @echo Muxing "%group% %enc_720_hi10%" && %MuxEp_720_x264% 
-if exist "%enc_1080_hi10%" if not exist "%group% %enc_1080_hi10%" @echo Muxing "%group% %enc_1080_hi10%" && %MuxEp_1080_x264% 
+REM Starts muxing and hashing
+echo Muxing and Hashing...
+echo. 
+if exist "%enc_480_h264%" if not exist "%group% %enc_480_h264:~0,-4%*.mkv" %MuxEp_480_x264% && %rhashSettings% "%group% %enc_480_h264%"
+if exist "%enc_720_hi10%" if not exist "%group% %enc_720_hi10:~0,-4%*.mkv" %MuxEp_720_x264% && %rhashSettings% "%group% %enc_720_hi10%"
+if exist "%enc_1080_hi10%" if not exist "%group% %enc_1080_hi10:~0,-4%*.mkv" %MuxEp_1080_x264% && %rhashSettings% "%group% %enc_1080_hi10%"
 
-if exist "%enc_720_hevc%" if not exist "%group% %enc_720_hevc%" @echo Muxing "%group% %enc_720_hevc%" && %MuxEp_720_x265% 
-if exist "%enc_1080_hevc%" if not exist "%group% %enc_1080_hevc%" @echo Muxing "%group% %enc_1080_hevc%" && %MuxEp_1080_x265% 
+if exist "%enc_720_hevc%" if not exist "%group% %enc_720_hevc:~0,-4%*.mkv" %MuxEp_720_x265% && %rhashSettings% "%group% %enc_720_hevc%"
+if exist "%enc_1080_hevc%" if not exist "%group% %enc_1080_hevc:~0,-4%*.mkv" %MuxEp_1080_x265% && %rhashSettings% "%group% %enc_1080_hevc%"
 
 REM pause
