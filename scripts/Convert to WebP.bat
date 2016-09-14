845388f02bc1ca43cd0a6d5061de55035a4ce603
C.A <anonymlol7@gmail.com>
2016-09-14 15:57:49 +0200

    upload ffmpeg, fix cwepb path

--- scripts/Convert to WebP.bat
+++ scripts/Convert to WebP.bat
@@ -2,6 +2,8 @@
 REM A short script to convert images to lossless WebP. Download the WebP tools here: https://developers.google.com/speed/webp/download
 REM To auto-delete the original files after the conversion, uncomment the 2 lines below (line 7 and 9).
 
+if exist C:\Doki_Tools\bin set PATH=%PATH%;C:\Doki_Tools\bin
+
 :loop
 echo Saving file "%~n1.webp"
 REM set input="%1"
