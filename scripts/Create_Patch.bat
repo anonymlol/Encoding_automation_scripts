857cb629bffbe05f41738eefafffcb22fecc2122
C.A <anonymlol7@gmail.com>
2017-06-07 11:20:35 +0200

    Create_Patch: Now chooses file input order automatically

--- scripts/Create_Patch.bat
+++ scripts/Create_Patch.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Usage: Select both files, but right-click/send-to (or drag & drop) only the source (unpatched) file!
+REM Usage: Select both files and right-click/send-to (or drag & drop) to the batch file!
 
 REM Set paths
 set xdelta_binary=C:\Doki_Tools\bin\xdelta3.exe
@@ -12,9 +12,22 @@ if not exist ".\Patches\xdelta3.exe" copy "%xdelta_binary%" ".\Patches"
 if not exist ".\Patches\Apply_Patch_Windows.bat" copy "%windows_script%" ".\Patches"
 if not exist ".\Patches\Apply_Patch_Linux_and_Mac.sh" copy "%linux_mac_script%" ".\Patches"
 
-echo Creating Patch...
-"%xdelta_binary%" -s "%~nx1" "%~nx2" "%~n1.delta"
-echo Done.
-move "%~n1.delta" ./Patches
+if "%~nx1" LSS "%~nx2" (
+    set source="%~nx1"
+    set target="%~nx2"
+    set patch="%~n1.delta"
+) else (
+    set source="%~nx2"
+    set target="%~nx1"
+    set patch="%~n2.delta"
+)
 
-REM pause
+echo Old: %source%
+echo New: %target%
+echo.
+echo Creating Patch...
+"%xdelta_binary%" -s %source% %target% %patch%
+echo Done.
+move %patch% ./Patches
+
+pause
